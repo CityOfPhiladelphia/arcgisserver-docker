2be3d33896f0ca602a29064d17381113f39531bd
EC2 Default User <ec2-user@ip-10-30-101-222.ec2.internal>
2026-05-18 16:14:39 +0000

    how to actually run it

--- run.sh
+++ run.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+set -e
+
+PASSWORD='apassword'
+sudo yum install -y nfs-utils
 
 docker build -t ags .
 
-docker volume create --driver local \
-  --opt type=nfs4 \
-  --opt o=addr=<efs-dns>,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport \
-  --opt device=:/arcgis/directories arcgis-directories
+docker stop ags 2>/dev/null || true
+docker rm ags 2>/dev/null || true
+docker volume rm arcgis-directories arcgis-config-store 2>/dev/null
 
-docker volume create --driver local \
-  --opt type=nfs4 \
-  --opt o=addr=<efs-dns>,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport \
-  --opt device=:/arcgis/config-store arcgis-config-store
+# make sure the EFS network tab has an NFS 2049/TCP firewall rule
+EFSDNS=<some-efs-dns-name>
+docker volume create \
+  --name arcgis-directories \
+  --driver local \
+  --opt type=nfs \
+  --opt o=addr=$EFSDNS,rw,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
+  --opt device=:/arcgis/directories
+
+docker volume create \
+  --name arcgis-config-store \
+  --driver local \
+  --opt type=nfs \
+  --opt o=addr=$EFSDNS,rw,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
+  --opt device=:/arcgis/config-store
 
 docker run -d \
   --restart unless-stopped \
@@ -18,7 +32,7 @@ docker run -d \
   -p 6443:6443 \
   -p 6080:6080 \
   -e ARCGIS_ADMIN_USER=siteadmin \
-  -e ARCGIS_ADMIN_PASS=apassword \
+  -e ARCGIS_ADMIN_PASS=$PASSWORD \
   -v arcgis-directories:/home/arcgis/server/usr/directories \
   -v arcgis-config-store:/home/arcgis/server/usr/config-store \
   ags:latest
